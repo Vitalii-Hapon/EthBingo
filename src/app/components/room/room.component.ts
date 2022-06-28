5e3051b5709b2a575873a9b8379f0dd54583142c
vitalii.hapon <GaponBibigon_2119>
2022-06-28 15:29:31 +0300

    finished game logic

--- src/app/components/room/room.component.ts
+++ src/app/components/room/room.component.ts
@@ -1,15 +1,16 @@
-import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { GameTime } from "../../models/models";
 import { interval, Observable, Subject } from "rxjs";
 import { map, takeUntil } from "rxjs/operators";
 import { Web3Service } from "../../services/web3.service";
 import { BalanceService } from "../../services/balance.service";
 import { TicketPriceService } from "../../services/ticket-price.service";
+import { SwiperOptions } from "swiper";
 
 const LimitForCountDown = 3;
-
-const Boards = [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}, {id: 6}, {id: 7}, {id: 8}, {id: 9}, {id: 10}];
-const calledBalls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const BoardsLength = 5;
+const BoardSize = 25;
+const CallSequenceLength = 75;
 
 @Component({
   selector: 'app-room',
@@ -17,35 +18,39 @@ const calledBalls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 
   styleUrls: ['./room.component.scss']
 })
 
-export class RoomComponent implements OnInit, OnDestroy {
+export class RoomComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('swiperContainer', { static: true }) swiperContainer!: ElementRef;
   public timeToStart!: number;
   public calledBalls: number[] = [];
-  public boards = Boards;
+  public boards!: number[][][];
+  public ticketPrice$: Observable<string> = this.ticketPriceService.ticketPrice$;
+  public balanceIsUpdated$: Observable<boolean> = this.balanceService.balanceIsIsUpdated$;
+  public ticketIsUpdated$: Observable<boolean> = this.ticketPriceService.ticketPriceIsUpdated$;
+  public balance$: Observable<string> = this.balanceService.balance$;
+  public callsSwiperConfig: SwiperOptions = {
+    slidesPerView: 10,
+    spaceBetween: 8,
+    direction: "horizontal",
+  };
+  public boardsSwiperConfig: SwiperOptions = {
+    slidesPerView: "auto",
+    spaceBetween: 25,
+    direction: "vertical",
+  };
+  private callSequence!: number[];
   // public boughTickets = 1;
   private currentGameTime!: GameTime;
   private stopCountDown$!: Subject<any>;
   private stopPlayTime$!: Subject<any>;
-  public ticket = Array.from({length: 5}, () => Array.from({length: 5}, () => Math.floor(Math.random() * 40)));
-  // public ticket = [
-  //   Array.from({length: 5}, () => Math.floor(Math.random() * 40)),
-  //   Array.from({length: 5}, () => Math.floor(Math.random() * 40)),
-  //   Array.from({length: 9}, () => Math.floor(Math.random() * 40))];
 
-
-  public ticketPrice$: Observable<string> = this.ticketPriceService.ticketPrice$;
-  public balanceIsUpdated$: Observable<boolean> = this.balanceService.balanceIsIsUpdated$;
-  public balance$: Observable<string> = this.balanceService.balance$;
+  public showStartNewGame!: boolean;
 
   constructor(
     private web3Service: Web3Service,
     private balanceService: BalanceService,
     private ticketPriceService: TicketPriceService,
-  ) { }
-
-  public balanceValueIsEnough(balanceInEther: string = '0,00108', ticketPriceInEther: string = '0,00108'): boolean {
-    const balanceAsBigint = Web3Service.convertToBigint(balanceInEther);
-    const ticketPriceAsBigint = Web3Service.convertToBigint(ticketPriceInEther);
-    return balanceAsBigint >= ticketPriceAsBigint;
+    private el: ElementRef,
+  ) {
   }
 
   public get isWonTime(): boolean {
@@ -62,6 +67,12 @@ export class RoomComponent implements OnInit, OnDestroy {
 
   public get isBuyTime(): boolean {
     return this.currentGameTime === GameTime.BuyTime
+  }
+
+  public balanceValueIsEnough(balanceInEther: string = '0,00108', ticketPriceInEther: string = '0,00108'): boolean {
+    const balanceAsBigint = Web3Service.convertToBigint(balanceInEther);
+    const ticketPriceAsBigint = Web3Service.convertToBigint(ticketPriceInEther);
+    return balanceAsBigint >= ticketPriceAsBigint;
   }
 
   public ngOnInit(): void {
@@ -82,11 +93,56 @@ export class RoomComponent implements OnInit, OnDestroy {
     this.initialize();
   }
 
+  public ballIsCalled(number: number): boolean {
+    return this.calledBalls.includes(number);
+  }
+
+  public trackByNumber(number: number): number {
+    return number;
+  }
+
   private initialize(): void {
+    this.showStartNewGame = false;
     this.currentGameTime = GameTime.BuyTime;
     this.stopPlayTime$ = new Subject();
     this.stopCountDown$ = new Subject();
     this.timeToStart = LimitForCountDown;
+    this.createCallSequence();
+    this.createBoards();
+  }
+
+  private createCallSequence() {
+    const nums: Set<number> = new Set();
+    while (nums.size !== CallSequenceLength) {
+      nums.add(Math.floor(Math.random() * 100) + 1);
+    }
+    this.callSequence = [...nums];
+  }
+
+  private createBoards() {
+    this.boards = [];
+
+    function chunkArray(myArray: number[], chunk_size: number): number[][] {
+      let index = 0;
+      const arrayLength = myArray.length;
+      const tempArray = [];
+
+      for (index = 0; index < arrayLength; index += chunk_size) {
+        const myChunk = myArray.slice(index, index + chunk_size);
+        tempArray.push(myChunk);
+      }
+
+      return tempArray;
+    }
+
+    for (let i = 0; i < BoardsLength; i++) {
+      const nums: Set<number> = new Set();
+      while (nums.size !== BoardSize) {
+        nums.add(Math.floor(Math.random() * 100) + 1);
+      }
+      const board = chunkArray([...nums], 5);
+      this.boards.push(board);
+    }
   }
 
   private startCountDown(): void {
@@ -110,13 +166,26 @@ export class RoomComponent implements OnInit, OnDestroy {
     interval(1000).pipe(
       takeUntil(this.stopPlayTime$),
       map(i => {
-        if (i === calledBalls.length) {
+        if (i === this.callSequence.length || this.winnerExist) {
           this.currentGameTime = GameTime.Won;
           this.stopPlayTime();
         }
-          this.calledBalls.unshift(calledBalls[i]);
+        this.calledBalls.unshift(this.callSequence[i]);
       })
     ).subscribe();
+  }
+
+  public get winnerExist(): boolean {
+    let winner = false;
+    for (const board of this.boards) {
+      for (const row of board) {
+        if (row.every(ball => this.calledBalls.includes(ball))) {
+          winner = true;
+          break;
+        }
+      }
+    }
+    return winner;
   }
 
   private finishGame(): void {
@@ -135,11 +204,14 @@ export class RoomComponent implements OnInit, OnDestroy {
     this.stopPlayTime$.complete();
   }
 
-  public ballIsCalled(number: number): boolean {
-    return this.calledBalls.includes(number);
+  ngAfterViewInit(): void {
+    if (this.el.nativeElement?.offsetHeight) {
+      this.boardsSwiperConfig.slidesPerView = this.el.nativeElement.offsetHeight / 270;
+    }
   }
 
-  public trackByNumber(number: number): number {
-    return number;
+  public closeWin(): void {
+    this.currentGameTime = GameTime.PLay
+    this.showStartNewGame = true;
   }
 }
